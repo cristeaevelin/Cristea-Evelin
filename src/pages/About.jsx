dc4bf4a801b145f65f315062680a0eb7e29849b0
cristeaevelin <cristeaevelin@gmail.com>
2025-11-23 23:09:41 +0200

    favicon

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,23 @@
 import TrueFocus from "../components/TrueFocus.jsx";
+import { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 export default function About() {
+  const pRef = useRef(null)
+
+  useEffect(() => {
+    gsap.fromTo(
+      pRef.current,
+      { opacity: 0, y: 30 },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 1.4,
+        delay: 0.7,
+        ease: "power3.out",
+      }
+    );
+  }, []);
 
   return (
     <div className="h-screen z-50 w-screen flex flex-col items-center border-b border-white/40 justify-center bg-[#1f1d1c]">
@@ -14,14 +31,14 @@ export default function About() {
       />
 
       <div className="space-y-10 text-2xl mx-20 text-[#F7E6DE] font-comic">
-        <p>
+        <p ref={pRef}>
           My journey started with a fascination for the intersection of art and
           technology. Today, I specialize in crafting immersive digital
           experiences that not only look stunning but also provide exceptional
           user experiences.
         </p>
 
-        <p>
+        <p ref={pRef}>
           When I'm not coding or designing, you'll find me exploring the latest
           design trends, experimenting with new technologies, or contributing to
           open-source projects.
